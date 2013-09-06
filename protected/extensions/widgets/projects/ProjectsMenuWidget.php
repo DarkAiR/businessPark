a7423e52587688bdb17d33122e56a1461c5a6b0d
DarkAiR <darkair@list.ru>
2013-09-07 02:48:14 +0600

    fix table names

--- protected/extensions/widgets/projects/ProjectsMenuWidget.php
+++ protected/extensions/widgets/projects/ProjectsMenuWidget.php
@@ -15,8 +15,8 @@ class ProjectsMenuWidget extends ExtendedWidget
 
         $items = Yii::app()->db->createCommand()
             ->select('count(*) as amount, sectionId, ps.name as name')
-            ->from('projects')
-            ->leftJoin('projectsections ps', 'ps.id = sectionId AND ps.visible=1')
+            ->from('Projects')
+            ->leftJoin('ProjectSections ps', 'ps.id = sectionId AND ps.visible=1')
             ->group('sectionId')
             ->where('projects.visible=1')
             ->queryAll();
