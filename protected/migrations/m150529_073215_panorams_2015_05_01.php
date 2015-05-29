34e2828d8f336150aac77b0072c471d75fec2e4d
DarkAiR <darkair@list.ru>
2015-05-29 22:42:20 +0500

    #40 новая панорама

--- protected/migrations/m150529_073215_panorams_2015_05_01.php
+++ protected/migrations/m150529_073215_panorams_2015_05_01.php
@@ -6,11 +6,12 @@ class m150529_073215_panorams_2015_05_01 extends CDbMigration
 	{
 		$this->execute("INSERT INTO `Panorama` (`createDate`, `swf`, `mov`, `visible`, `version`) VALUES ('2015-05-01', 'a.swf', '', 1, 2)");
 		$this->execute("INSERT INTO `Panorama` (`createDate`, `swf`, `mov`, `visible`, `version`) VALUES ('2015-05-01', 'b.swf', '', 1, 2)");
+		$this->execute("INSERT INTO `Panorama` (`createDate`, `swf`, `mov`, `visible`, `version`) VALUES ('2015-05-01', 'c-1.swf', '', 1, 2)");
 		$this->execute("INSERT INTO `Panorama` (`createDate`, `swf`, `mov`, `visible`, `version`) VALUES ('2015-05-01', 'd2-1.swf', '', 1, 2)");
 	}
 
 	public function down()
 	{
-		$this->execute("DELETE FROM `Panorama` WHERE `createDate`='2015-05-01' AND `swf` IN ('a.swf', 'b.swf', 'd2-1.swf')");
+		$this->execute("DELETE FROM `Panorama` WHERE `createDate`='2015-05-01' AND `swf` IN ('a.swf', 'b.swf', 'c-1.swf', 'd2-1.swf')");
 	}
 }
